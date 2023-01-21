93243f8c261a89a8319481afb611723d80b4e515
SPoluliakh <106100426+SPoluliakh@users.noreply.github.com>
2023-01-21 10:44:45 +0000

    Deploying to gh-pages from @ SPoluliakh/helper@50bff4df11880481ffe35adb23067a8e25188b71 🚀

---
+++ static/js/126.fa86e2d2.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkhelper=self.webpackChunkhelper||[]).push([[126],{9126:function(t,a,e){e.d(a,{C7Q:function(){return l}});var r=e(9983);function l(t){return(0,r.w_)({tag:"svg",attr:{fill:"currentColor",viewBox:"0 0 16 16"},child:[{tag:"path",attr:{d:"M1.293 1.293a1 1 0 0 1 1.414 0L8 6.586l5.293-5.293a1 1 0 1 1 1.414 1.414L9.414 8l5.293 5.293a1 1 0 0 1-1.414 1.414L8 9.414l-5.293 5.293a1 1 0 0 1-1.414-1.414L6.586 8 1.293 2.707a1 1 0 0 1 0-1.414z"}}]})(t)}}}]);
+//# sourceMappingURL=126.fa86e2d2.chunk.js.map
